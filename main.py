60356f848ae9a9fe0ea01733543364f281085b06
edevars <e.devars.dev@gmail.com>
2019-09-02 21:30:43 -0500

    :sparkles: Adding method to read books

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+def leer():
+    file = open("./books/libro1.txt","r")
+    contenido = list()
+    if file.mode == 'r':
+        contenido = file.read()
+    
+    return contenido
+
 if __name__== "__main__":
-  print('hola mundo!')
+    contenido = leer()
+    print(contenido)
